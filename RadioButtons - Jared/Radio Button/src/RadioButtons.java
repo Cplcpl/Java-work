bcfe06500048f6e09856dfce2d1630a591161b93
Jared Kimbel <1255376@ad.ccsd.net>
2017-01-09 09:36:50 -0800

    Completed RadioButtons Project

---
+++ RadioButtons - Jared/Radio Button/src/RadioButtons.java
@@ -0,0 +1,79 @@
+import java.awt.*; 
+import java.awt.event.*;
+import javax.swing.*; 
+public class RadioButtons extends JFrame 
+                          implements ActionListener
+{
+   private JTextField textField;
+   private JRadioButton radioPlain;
+   private JRadioButton radioItalic;
+   private JRadioButton radioBold;
+
+   public static void main ( String[] args )
+   {
+      new RadioButtons() ;
+   }
+
+   // constructor 
+   public RadioButtons() 
+   {
+      JFrame myFrame = new JFrame(); 
+      myFrame.setLayout(new FlowLayout()); 
+      myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      JPanel mainPanel = (JPanel)myFrame.getContentPane();
+
+      // create ButtonGroup and new JRadioButtons
+      ButtonGroup radioGroup = new ButtonGroup();
+      radioPlain = new JRadioButton("Plain");
+      radioItalic = new JRadioButton("Italic");
+      radioBold = new JRadioButton("Bold");
+
+      // select the plain radio button by default
+      radioPlain.setSelected(true);
+
+      // add radio buttons to group
+      radioGroup.add(radioPlain);
+      radioGroup.add(radioItalic);
+      radioGroup.add(radioBold);
+
+      // add radio buttons to panel
+      mainPanel.add(radioPlain);
+      mainPanel.add(radioItalic);
+      mainPanel.add(radioBold);
+
+      // register action listener for each button
+      radioPlain.addActionListener(this);
+      radioItalic.addActionListener(this);
+      radioBold.addActionListener(this);
+
+      // create and add text field to panel
+      textField = new JTextField( "RadioButton font", 
+                                  25 );
+      mainPanel.add(textField);
+      myFrame.pack();
+      myFrame.setVisible(true);
+   }
+
+   public void actionPerformed( ActionEvent event)
+   {
+      Object control = event.getSource();
+      int style; // this will hold the selected font
+
+      // check which command has been sent
+      if ( radioPlain.isSelected() )
+      {
+         style = Font.PLAIN;
+      }
+      else if (radioItalic.isSelected() )
+      {
+         style = Font.ITALIC;
+      }
+      else
+      {
+         style = Font.BOLD;
+      }
+      // update font based on selected style
+      Font font = new Font( "TimesRoman", style, 14 );
+      textField.setFont( font );
+   }
+}
